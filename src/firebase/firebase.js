3dcefad911bb8a67c17d66a599d9aa6e47d83448
SUPINFO AppFactory <MiladiMed@MacBookRetina.local>
2017-12-20 23:54:51 +0100

    adding firebase auth step 1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,9 +13,10 @@ var config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 
-export { firebase, database as default}
+export { firebase, googleAuthProvider, database as default}
 
 // database.ref('expenses').on('value', (snapshots) => {
 //     const expenses = [];
